21157b0daba3f8023e4a1126688ba077b319ac72
ao-09blu <a.namai@net.lab.uec.ac.jp>
2022-12-12 01:52:19 -0800

    web3&ethのセットアップ機能追加

---
+++ settings/install-nodejs.sh
@@ -0,0 +1,4 @@
+#!/user/bin/bash
+sudo apt install nodejs
+nodejs -v
+sudo apt install npm
---
+++ settings/setup.sh
@@ -0,0 +1,5 @@
+#!/user/bin/bash
+bash web3install.sh
+bash geth-install.sh
+bash install-nodejs.sh
+bash install-pysolc.sh
