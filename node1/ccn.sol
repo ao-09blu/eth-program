f993e99e938d8c8165c043de62a22b071f9d895c
ao-09blu <a.namai@net.lab.uec.ac.jp>
2023-03-28 01:24:15 -0700

    node1追加

---
+++ node1/ccn.sol
@@ -0,0 +1,78 @@
+pragma solidity >=0.5.16;
+
+//配列を返す&入力できるように
+pragma experimental ABIEncoderV2;
+
+contract Greeter {
+    //必要な情報の宣言
+    //コンテンツ情報の登録
+    struct Contents_Inf{
+        address producer; //コンテンツの配信者のアドレス
+        uint[] holder;//コンテンツホルダーのルータID
+        string[] keyword;
+    }
+    //コンテンツ配信者の登録
+    struct Producer{
+        string Pname;
+        address ip_add;
+    }
+    struct Router{
+        uint RouterID;
+        string[] content_URI;
+        string[] neighbor; 
+    }
+    mapping(string=>Contents_Inf) public content_array;
+    mapping(address=>Router) public Link_DB;
+    uint public ID = 0;
+    address[] R_add;//添字がルータID,中身がそのルータのaddress
+    bool[] flag;
+    uint[] hop; //ホップ数
+    //関数
+
+    //新規コンテンツの登録
+    function regist_content(string memory _URI, uint[] memory _holder, string[] memory _keyword) public returns(uint){
+        content_array[_URI].producer = msg.sender;
+        content_array[_URI].holder = _holder;
+        content_array[_URI].keyword = _keyword;
+    }
+
+    //新規ルータ追加
+    function regist_router(string[] memory _URI, string[] memory _neighbor)public returns(uint){
+        Link_DB[msg.sender].RouterID = ID;
+        Link_DB[msg.sender].content_URI = _URI;
+        Link_DB[msg.sender].neighbor = _neighbor;
+        R_add.push(msg.sender);
+        ID = ID + 1;
+        flag.push(false);
+        return Link_DB[msg.sender].RouterID;
+    }
+
+    function update_router(string[] memory _URI, string[] memory _neighbor) public{
+        Link_DB[msg.sender].content_URI = _URI;
+        Link_DB[msg.sender].neighbor = _neighbor;
+    }
+
+    function update_topology(string[] memory _URI, string[] memory _neighbor) public{
+        
+    }
+
+    function search_start(uint _RID) public{
+        for(uint i=0; i<flag.length; i++){
+            hop[i] = 0;
+            if(i==_RID){
+                flag[i] = true;
+            }
+            else{
+                flag[i] = false;
+            }
+        }
+    }
+    /*
+    function show_contents() view public returns (Contents_info[] memory) {
+        uint list_length = contents_list.length;
+        Contents_info[] memory content_array = new Contents_info[](list_length);
+        content_array = contents_list;
+        return content_array;
+    }
+    */
+}
